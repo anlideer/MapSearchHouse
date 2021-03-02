6cacc1337e8c9ce4850ce0db9677e49174d1f1ac
anlideer <643525796@qq.com>
2021-03-02 20:47:18 +0800

    找了个模板，给header做了一点点修改

---
+++ N3-admin-master/src/layout/header.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="app-header clearfix">
+    <n3-column :col="10" class="logo-field">
+      <img src="../assets/logo.png" class="logo"/> <span>地图找房</span>
+    </n3-column>
+    <n3-colum class="search-box">
+      <n3-input v-model="keyword"></n3-input>
+      <img src="../assets/images/search.png" class="search-logo" @click.native="changeLocation"/>
+    </n3-colum>
+    <n3-column class="logout-field">
+      <n3-tooltip class="tip-logout" content="登出" placement="bottom" trigger="hover">
+        <n3-icon size="20px" type="sign-out" @click.native="submit"></n3-icon>
+      </n3-tooltip>
+    </n3-column>
+  </div>
+</template>
+
+<script>
+  import { mapActions } from 'vuex'
+  export default {
+    methods: {
+      ...mapActions(['logout']),
+      submit () {
+        this.logout()
+        this.$router.push({
+          name: 'login'
+        })
+      }
+    }
+  }
+</script>
+
+<style lang="less">
+  .app-header {
+    height: 60px;
+    line-height: 60px;
+    background: #fff;
+    color: #c0ccda;
+    display: flex;
+    position: fixed;
+    width:100%;
+    z-index:3;
+    box-shadow: 0px 0px 5px #f9f9f9;
+    .fixed {
+      position: absolute;
+      top: 0px;
+      bottom: 0px;
+      width: 100%;
+    }
+    .logo-field {
+      flex: 1;
+      font-size: 26px;
+    }
+    .logo {
+      width: 40px;
+      float: left;
+      margin: 10px 10px 10px 18px;
+    }
+    .search-logo {
+      width: 25px;
+      margin: 10px 10px 10px 10px;
+    }
+    .logout-field {
+      width: 40px;
+      padding-right: 20px;
+      padding-top: 5px;
+      i {
+        cursor: pointer;
+      }
+    }
+    .username {
+      font-size: 18px;
+    }
+    .search-box {
+      padding-right: 650px;
+      padding-top: 5px;
+      float: left;
+    }
+  }
+</style>
